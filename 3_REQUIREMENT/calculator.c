3755a4c4fc5f787a1e33d7452ddd2ec9da50ac0f
Dheeraj944 <158841476+Dheeraj944@users.noreply.github.com>
2024-05-17 07:06:35 +0000

    Changes

--- 3_REQUIREMENT/calculator.c
+++ 3_REQUIREMENT/calculator.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-  int num1, num2, difference;
+  int num1, num2, Sum;
   printf("Enter first number: ");
   scanf("%d", &num1);
   printf("Enter second number: ");
   scanf("%d", &num2);  
-  difference = num1 - num2;
-  printf("Difference of %d and %d is: %d", num1, num2, difference);
+  Sum = num1 + num2;
+  printf("Sum of %d and %d is: %d", num1, num2, Sum);
   return 0;
 }
